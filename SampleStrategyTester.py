334e9988e3e0f7541076346608f4c84e67702215
keyurjk <39686096+keyurjk@users.noreply.github.com>
2021-06-06 15:45:35 +0530

    SampleStrategyTester.py
    created

---
+++ SampleStrategyTester.py
@@ -0,0 +1,5 @@
+# First sample code on Jupyter Notebook to test a trading strategy
+# data imported from csv file of USDINR future from NSE for 2020 year
+import pandas as pd
+
+print('Hello World!')
