4fc44b511160498fbe8ff017883e4f5ba693651c
keyurjk <39686096+keyurjk@users.noreply.github.com>
2021-07-23 11:48:59 +0530

    Nifty index file read

--- csvReader.py
+++ csvReader.py
@@ -4,11 +4,13 @@ import statistics
 version1: read bhavcopy files of last 2 days and today live stock data file and mearge in a list and print.
 version1.1: collect data in a map. with stokname as a key and two day historical data + live data as list for the stock.
 version2: StrategyBuilder with 2DH BO bullish strategy coded and verified actual run.
+version2.1: Nifty index dataanalystics added. (23-July-2021)
 version3: TODO 1. To livetest, afternoon data and EOD data. 2. generate report.
             3. Improve on Entry exit. (halfway entry, SL-calc first and calculate Target based on SL and if matching Daily pivot.
             4. Improve execution by adding QTY to profitable trades and reducing for losing.
             5. Also book profit at 1% gain from entry or (2% gain of stock) or R2 touch.
-version4: Modify for backtest data of last one year.            
+version4: TODO Modify for backtest data of last one year.
+
 '''
 
 class StrategyTester:
@@ -179,4 +181,96 @@ class FileReader:
 ##        ret_col = [col[7] for col in lt]
 ##        dif_col = [col[8] for col in lt]
 ##        print('Total count:', len(ret_col), ': average ret %--', round((sum(ret_col)/len(ret_col)),2), 'stdev of dif--', round(statistics.stdev(dif_col),2))
+
+
+    def readNiftyIndexDataFile(self, fileName):
+
+        with open(fileName, newline='') as f:
+            reader = csv.reader(f)
+            print('Date, Close Price')
+            lt = list(reader)
+            del lt[0]
+            self.dataAnalytics(self, lt)
+           
+
+    def dataAnalytics(self, lt):
+        fromdt = lt[0][0]
+        ltlen = len(lt)-1
+        todt = lt[ltlen][0]
+        print('Full Data analysis:',fromdt, todt)
+        
+        self.calcDailyClosePPReturn(self, lt)
+        self.calcDailyOpenToHighReturn(self, lt)
+        self.calcDailyOpenToLowReturn(self, lt)
+        self.calcDailyATRret(self, lt)
+
+        next2Index = int(round((ltlen/3)*2, 0))
+        fromdt2 = lt[next2Index][0]        
+        print('1/3rd Recent Data analysis:',fromdt2, todt)
+        self.calcDailyClosePPReturn(self, lt[next2Index:ltlen])
+        self.calcDailyOpenToHighReturn(self, lt[next2Index:ltlen])
+        self.calcDailyOpenToLowReturn(self, lt[next2Index:ltlen])
+        self.calcDailyATRret(self, lt[next2Index:ltlen])
+
+        next3Index = int(round((ltlen/5)*4, 0))
+        fromdt3 = lt[next3Index][0]        
+        print('Recent Data analysis:',fromdt3, todt)
+        self.calcDailyClosePPReturn(self, lt[next3Index:ltlen])
+        self.calcDailyOpenToHighReturn(self, lt[next3Index:ltlen])
+        self.calcDailyOpenToLowReturn(self, lt[next3Index:ltlen])
+        self.calcDailyATRret(self, lt[next3Index:ltlen])
+        
+        
+    def calcDailyClosePPReturn(self, lt):
+    
+        last_cp = float(lt[0][4])
+        dif_col = []
+        ret_col = []
+        for row in lt:
+            dt = row[0]
+            cp = float(row[4])
+            dif = round(cp - last_cp,2)
+            #print('----', dt, cp, last_cp, dif)
+            ret = round((dif/last_cp)*100,1)
+                
+            dif_col.append(dif)
+            ret_col.append(abs(ret))
+            last_cp = cp
+            
+        print('calcDailyClosePPReturn:')
+        print('Total count:', len(ret_col), ': average ret %--', round((sum(ret_col)/len(ret_col)),2), 'stdev of dif--', round(statistics.stdev(dif_col),2))
+
+    def calcDailyOpenToHighReturn(self, lt):
+        print('calcDailyOpenToHighReturn:')
+        self.calcGenericRet(self, lt, 1, 2, 1)
+        
+        
+    def calcDailyOpenToLowReturn(self, lt):
+        print('calcDailyOpenToLowReturn:')
+        self.calcGenericRet(self, lt, 1, 1, 3)
+        
+        
+    def calcDailyATRret(self, lt):
+        print('calcDailyATRret:')
+        self.calcGenericRet(self, lt, 1, 2, 3)
+        
+        
+    def calcGenericRet(self, lt, index_1, index_2, index_3):
+
+        dif_col = []
+        ret_col = []                
+        for row in lt:
+            dt = row[0]
+            openP = float(row[index_1])
+            highP = float(row[index_2])
+            lowP = float(row[index_3])
+            dif = round(highP - lowP,2)
+            #print('----', dt, openP, highP, dif)
+            ret = round((dif/openP)*100,1)
+                
+     
+            dif_col.append(dif)
+            ret_col.append(abs(ret))
+            
+        print('Total count:', len(ret_col), ': average ret %--', round((sum(ret_col)/len(ret_col)),2), 'stdev of dif--', round(statistics.stdev(dif_col),2))
         
